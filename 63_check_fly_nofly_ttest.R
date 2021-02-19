f12a6c6501feb44e4d9794365fa7d0dca8bae23a
aliceziegler <alice.zglr@gmail.com>
2021-02-19 16:21:48 +0100

    deleted comented unused code in scripts

--- 63_check_fly_nofly_ttest.R
+++ 63_check_fly_nofly_ttest.R
@@ -1,7 +1,7 @@
 # Description:
 # Author: Alice Ziegler
 # Date: 2018-12-10 11:49:55
-# t-test: check if RMSE/sd resultes for elevation as best model difer between flying and non-flying animals
+#
 rm(list=ls())
 
 ########################################################################################
@@ -12,29 +12,10 @@ rm(list=ls())
 #####
 library(pastecs)
 source("000_setup.R")
-# #####
-# ###set paths
-# #####
-# setwd(dirname(rstudioapi::getSourceEditorContext()[[2]]))
-# # setwd("/mnt/sd19006/data/users/aziegler/src")
-# # sub <- "oct19/" 
-# #paper: 
-# # sub <- "apr19/"
-# sub <- "feb20_allresp/"
-# inpath <- paste0("../data/", sub)
-# inpath_general <- "../data/"
-# outpath <- paste0("../out/", sub)
-# #####
-# ###where are the models and derived data
-# #####
-# # set_dir <- "2019-10-10frst_nofrst_allplts_noelev/" 
-# #paper: 
-# # set_dir <- "2019-03-26frst_nofrst_allplts_noelev/"
-# set_dir <- "2020-02-12frst_nofrst_allplts_noelev/"
-# 
-# 
-# mod_dir_lst <- list.dirs(path = paste0(inpath, set_dir), recursive = F, full.names = F)
-# modDir <- paste0(inpath, set_dir, "mix/")
+
+#####
+### Settings
+#####
 comm <- ""
 grp <- c("specs", "trophs")
 trophs <- c("generalist", "herbivore", "decomposer", "predator")
@@ -43,7 +24,6 @@ trophs <- c("generalist", "herbivore", "decomposer", "predator")
 #####
 
 mix_lst <- readRDS(file = paste0(modDir, "data/", "61_master_lst_varimp_.rds")) 
-
 
 troph_mrg <- readRDS(paste0(inpath, "15_troph_mrg.rds"))
 troph_mrg <- troph_mrg[!duplicated(troph_mrg),]
@@ -66,7 +46,7 @@ size <- data.frame(response = c("SRants", "SRbats", "SRbees", "SRbirds", "SRcoll
 ########################################################################################
 
 #####
-###Tabellen?bersicht der RMSE_sd Median Werte
+###table overview of RMSE_sd Median values
 #####
 results_summary <- val_troph[!(val_troph$resp %in% c("sum_decomposer_N3", "sum_generalist_N4", 
                                                      "sum_herbivore_N3", "sum_predator_N5")), 
@@ -112,77 +92,3 @@ fly_size_tests <- fly_size_tests[c(2,3,1,4),]
 
 
 write.csv(fly_size_tests, file = paste0(outpath, "fly_size_test.csv"))
-
-# 
-# 
-# ###############################
-# # data file = body_size_fly.csv
-# # data<- read.csv2("body_size_fly.csv")
-# # attach(data)
-# fly<-as.factor(fly)
-# 
-# ############################
-# # Spearman correlation tests
-# cor.test(results_mdn$size, results_mdn$fig3_elev_median)
-# cor.test(rank_size,Fig3a_struc)
-# cor.test(rank_size,Fig3a_combi)
-# cor.test(rank_size,Fig4)
-# 
-# ############################
-# # U-tests tests
-# wilcox.test(results_mdn$fig3_elev_median~results_mdn$fly)
-# wilcox.test(Fig3a_struc~fly)
-# wilcox.test(Fig3a_combi~fly)
-# wilcox.test(Fig4~fly)
-# 
-# 
-# 
-# 
-# 
-# 
-# # val_troph, where elevation is best model
-# val_troph_elev <- val_troph[(val_troph$RMSEsd_elevSR_mdn_rank == 1 | 
-#                               #hier | statement, weil auch das lidar UND elevation auf 1 sein kann. 
-#                               #da das inzwischen nciht mehr verwendet wird, w?re das elev aber unter 
-#                               #umst?nden trotzdem noch das beste
-#                               (val_troph$RMSEsd_lidarelevSR_mdn_rank == 1 & 
-#                                  val_troph$RMSEsd_elevSR_mdn_rank == 2)),
-#                             c("resp", "run", "RMSEsd_elevSR", "RMSEsd_elevSR_mdn", "Tax_label", "fly", "troph_sep")]
-# # only flying
-# fly <- val_troph_elev[val_troph_elev$fly == 1,]
-# # only noflying
-# nofly <- val_troph_elev[val_troph_elev$fly == 0,]
-# 
-# #######################
-# ###test all RMSEsd_elevSR valueas against each other
-# #######################
-# 
-# #####
-# ###explore data
-# #####
-# shapiro.test(fly$RMSEsd_elevSR) 
-# hist(fly$RMSEsd_elevSR)
-# 
-# shapiro.test(nofly$RMSEsd_elevSR) 
-# hist(nofly$RMSEsd_elevSR)
-# 
-# by(val_troph_elev$RMSEsd_elevSR, val_troph_elev$fly, stat.desc)
-# #####
-# ###test 
-# #####
-# t.test(fly$RMSEsd_elevSR, nofly$RMSEsd_elevSR)
-# 
-# #######################
-# ###test all RMSEsd_elevSR_mdn valueas against each other(only median RMSEs)
-# #######################
-# shapiro.test(fly$RMSEsd_elevSR_mdn) 
-# hist(fly$RMSEsd_elevSR_mdn)
-# 
-# shapiro.test(nofly$RMSEsd_elevSR_mdn) 
-# hist(nofly$RMSEsd_elevSR_mdn)
-# 
-# by(val_troph_elev$RMSEsd_elevSR_mdn, val_troph_elev$fly, stat.desc)
-# #####
-# ###test 
-# #####
-# t.test(fly$RMSEsd_elevSR_mdn, nofly$RMSEsd_elevSR_mdn)
--- 71_plotting_frst_nofrst_inone.R
+++ 71_plotting_frst_nofrst_inone.R
@@ -25,26 +25,11 @@ library(pBrackets)
 library(gtable)
 library(cowplot)
 source("000_setup.R")
-# 
-# 
-# #####
-# ###set paths
-# #####
-# setwd(dirname(rstudioapi::getSourceEditorContext()[[2]]))
-# # setwd("/mnt/sd19006/data/users/aziegler/src")
-# sub <- "feb20_allresp/"
-# # sub <- "apr19/" #paper
-# inpath <- paste0("../data/", sub)
-# inpath_general <- "../data/"
-# outpath <- paste0("../out/", sub)
+
 #####
-###where are the models and derived data
+###settings
 #####
-# 
-# set_dir <- "2020-02-12frst_nofrst_allplts_noelev/"
-# # set_dir <- "2019-03-26frst_nofrst_allplts_noelev/"#paper
-# 
-# mod_dir_lst <- list.dirs(path = paste0(inpath, set_dir), recursive = F, full.names = F)
+
 modDir <- paste0(inpath, set_dir, "mix/")
 comm <- ""
 grp <- c("specs", "trophs")
@@ -71,13 +56,7 @@ val_all$resp <- do.call(c,resp_nm)
 val_troph <- merge(val_all, troph_mrg, by = "resp") 
 val_troph$troph_sep[grepl("sum", val_troph$resp)] <- paste0(val_troph$diet[grepl("sum", val_troph$resp)], "_sum")
 val_troph$troph_sep[is.na(val_troph$troph_sep)] <- as.character(val_troph$diet[is.na(val_troph$troph_sep)])
-# val_troph$troph_sep <- factor(val_troph$troph_sep, 
-#                               levels = c("predator", "predator_sum", "generalist", 
-#                                          "generalist_sum", "decomposer", 
-#                                          "decomposer_sum", "herbivore", "herbivore_sum", 
-#                                          "plant", "plant_sum", "birds", "bats"))
-# val_troph$resp <- factor(val_troph$resp, 
-# levels = unique(val_troph[with(val_troph, order(troph_sep, resp)),"resp"]))
+
 val_troph$diet <- factor(val_troph$diet, 
                          levels = levels(val_troph$diet))
 
@@ -147,12 +126,9 @@ val_type$type_col[grepl("lidarSR", val_type$type)] <- "dodgerblue4"
 val_type$type_col[grepl("lidarelevSR", val_type$type)] <- "darkorchid3"
 val_type$type_col[grepl("lidarRES", val_type$type)] <- "goldenrod1"
 
-
-
 #####
 ###sort resp by troph levels for further operations
 #####
-
 
 sort_by_diet_alphabet <- (val_type %>% distinct(diet, .keep_all = TRUE))[match(sort(levels(droplevels(val_type$diet))), (val_type %>% distinct(diet, .keep_all = TRUE))[,which(colnames(val_type %>% distinct(diet, .keep_all = TRUE)) == "diet")]),]
 myColors <- c(as.character(sort_by_diet_alphabet$color), "black", "grey70", "grey30", "grey90", "white")
@@ -165,8 +141,7 @@ n <- "RMSEsd_"
   rank_df <- rank_df[,grepl("_elevSR", colnames(rank_df)) | 
                        grepl("_sumSR", colnames(rank_df)) |
                        grepl("_lidarSR", colnames(rank_df))]
-  # val_plt$szenario <- colnames(rank_df)[]
-  
+
   for(row_nr in seq(nrow(val_plt))){
     min_colnm <- colnames(rank_df)[rank_df[row_nr,] == min(rank_df[row_nr,])]
     val_plt$best_mod[row_nr] <- strsplit(min_colnm, "_")[[1]][2]
@@ -188,9 +163,7 @@ n <- "RMSEsd_"
   levels(val_plt_flt$resp) <- levels(val_type$resp)
   levels(val_plt_flt$diet) <- levels(val_type$diet)
   
-  # val_plt_flt$troph_sep <- droplevels(val_plt_flt$troph_sep)
   val_plt_flt$color <- droplevels(val_plt_flt$color)
-  # val_plt_flt$resp <- droplevels(val_plt_flt$resp)
   val_plt_flt$diet <- droplevels(val_plt_flt$diet)
   
   table <- aggregate(val_plt_flt$value, by = list(val_plt_flt$Taxon, val_plt_flt$type), FUN = median)
@@ -217,14 +190,8 @@ n <- "RMSEsd_"
     ggtitle(paste0(sub, "_", n))
   pdf(file = paste0(outpath, set_dir, "mix/val_plot_srt_trophicslvl_", comm, n, ".pdf"), height= 10, 
       width = 20)
-  # par(mar=c(50, 50, 50, 50) + 1)#, paper = "a4r")
   print(p)
   dev.off()
-  # png(paste0(modDir, "/val_plot_srt_trophicslvl_", comm, n, names(set_lst)[cnt], ".png"), 
-  #     width = 297, height = 210, units = "mm", res = 720)
-  # # par(mai=c(50, 50, 50, 50) + 1)
-  # print(p)
-  # dev.off()
 
 #####
 ###best model sorting
@@ -272,8 +239,6 @@ n <- "RMSEsd_"
             axis.ticks.y = element_blank(),
             axis.ticks.x = element_blank(),
             axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)))+
-      # theme_bw()+
-      # colour = dat$troph_sep,
       geom_text(data = dat_text, mapping = aes(x = rep(0.5, nrow(dat_text)), y = rep(4.5, nrow(dat_text)), label = label, angle = 90),
                 hjust   = 1, vjust   = 1, size = (20*0.352777778))+ #divide by pts per inch
       labs(x = "", y = "RMSE/sd")+
@@ -341,9 +306,6 @@ n <- "RMSEsd_"
   write.csv(trophs_ord, paste0(outpath, set_dir, "mix/val_plot_srt_bestmodel_", comm, n, "trophs.csv"))
   
 
-  # a <- plot_grid(specs_plt, trophs_plt, leg, labels = c('A', 'B'), label_size = 12)
-  # # a <- arrangeGrob(specs_plt, trophs_plt, leg, ncol=3, widths=c(2.3, 2.3, 0.8))
-  # 
   ##printing
   # specs
   pdf(file = paste0(outpath, set_dir, "mix/val_plot_srt_bestmodel_", comm, n, "specs.pdf"), height= 11, width = 22)
@@ -353,139 +315,11 @@ n <- "RMSEsd_"
   pdf(file = paste0(outpath, set_dir, "mix/val_plot_srt_bestmodel_", comm, n, "trophs.pdf"), height= 9.8, width = 7)
   print(trophs_plt)
   dev.off()
-  # legend #to be cropped outside R
-  # pdf(file = paste0(outpath, set_dir, "mix/val_plot_srt_bestmodel_", comm, n, "legend.pdf"))
-  # plot(leg)
-  # dev.off()
+  legend #to be cropped outside R
+  pdf(file = paste0(outpath, set_dir, "mix/val_plot_srt_bestmodel_", comm, n, "legend.pdf"))
+  plot(leg)
+  dev.off()
 
-  # both
-  # pdf(file = paste0(outpath, set_dir, "mix/val_plot_srt_bestmodel_", comm, n, "both.pdf"))
-  # print(a)
-  # dev.off()
-  
-  
-
-  
-  
-  
-  # a <- plot_grid(specs_plt, trophs_plt, leg, labels = c('A', 'B'), label_size = 12)
-  # # a <- arrangeGrob(specs_plt, trophs_plt, leg, ncol=3, widths=c(2.3, 2.3, 0.8))
-  # # both
-  # pdf(file = paste0(outpath, set_dir, "mix/val_plot_srt_bestmodel_", comm, n, "both.pdf"))
-  # print(a)
-  # dev.off()
-  
-  
-  # 
-  ###
-  #reorder by median performance of each facet (elevation facet ordered by elevation perf., structure facet ordered by structure,...)
-  ###
-  # unq_mdn <- unique(data.frame(Tax_label = val_plt_grp$Tax_label,
-  #                              RMSEsd_elevSR_mdn = val_plt_grp$RMSEsd_elevSR_mdn,
-  #                              RMSEsd_lidarSR_mdn = val_plt_grp$RMSEsd_lidarSR_mdn,
-  #                              RMSEsd_sumSR_mdn = val_plt_grp$RMSEsd_sumSR_mdn,
-  #                              best_mod = val_plt_grp$best_mod))
-  # unq_mdn$Tax_label <- as.character(unq_mdn$Tax_label)
-  # 
-  # srt_mdn <- c()
-  # lookup <- data.frame(best_mod = c("elevation", "structure", "combination"),
-  #                      title = c("RMSEsd_elevSR_mdn", "RMSEsd_lidarSR_mdn", "RMSEsd_sumSR_mdn"))
-  # lookup[] <- lapply(lookup, as.character)
-  # for(o in levels(val_plt_grp$best_mod)){
-  #   #o <- "elevation"
-  #   sub_tmp <- unq_mdn[unq_mdn$best_mod == o,]
-  #   title_tmp <- lookup$title[lookup$best_mod == o]
-  #   srt_tmp <- sub_tmp[order(sub_tmp[as.character(title_tmp)]),]
-  #   srt_mdn <- c(srt_mdn, rev(srt_tmp$Tax_label))
-  # }
-  # val_plt_grp$Tax_label <- factor(val_plt_grp$Tax_label, levels = srt_mdn)
-  # 
-  # 
-  # 
-  # for (i in grp){ #i <- "specs", i <- "trophs"
-  #   if(i == "specs"){
-  #     val_plt_grp <- val_plt_flt[!val_plt_flt$Taxon %in% trophs,]
-  #     width = 22
-  #   } else if(i == "trophs"){
-  #     width = 7.5
-  #     val_plt_grp <- val_plt_flt[val_plt_flt$Taxon %in% trophs,]
-  #   }
-
-  #   ###
-  #   #reorder by median performance of each facet (elevation facet ordered by elevation perf., structure facet ordered by structure,...)
-  #   ### 
-    # unq_mdn <- unique(data.frame(Tax_label = val_plt_grp$Tax_label,
-    #                                RMSEsd_elevSR_mdn = val_plt_grp$RMSEsd_elevSR_mdn,
-    #                                RMSEsd_lidarSR_mdn = val_plt_grp$RMSEsd_lidarSR_mdn,
-    #                                RMSEsd_sumSR_mdn = val_plt_grp$RMSEsd_sumSR_mdn,
-    #                                best_mod = val_plt_grp$best_mod))
-    # unq_mdn$Tax_label <- as.character(unq_mdn$Tax_label)
-    # 
-    # srt_mdn <- c()
-    # lookup <- data.frame(best_mod = c("elevation", "structure", "combination"),
-    #                      title = c("RMSEsd_elevSR_mdn", "RMSEsd_lidarSR_mdn", "RMSEsd_sumSR_mdn"))
-    # lookup[] <- lapply(lookup, as.character)
-    # for(o in levels(val_plt_grp$best_mod)){
-    #   #o <- "elevation"
-    #   sub_tmp <- unq_mdn[unq_mdn$best_mod == o,]
-    #   title_tmp <- lookup$title[lookup$best_mod == o]
-    #   srt_tmp <- sub_tmp[order(sub_tmp[as.character(title_tmp)]),]
-    #   srt_mdn <- c(srt_mdn, rev(srt_tmp$Tax_label))
-    # }
-    # val_plt_grp$Tax_label <- factor(val_plt_grp$Tax_label, levels = srt_mdn)
-
-  #   
-  #   plt <-
-  #     ggplot() +
-  #     geom_boxplot(data = val_plt_grp, aes(x=Tax_label, y=value, fill=type), notch = T) +   #in aes(position=position_dodge(5))
-  #     facet_grid(~best_mod, scales = "free_x", space="free_x") +
-  #       # theme_minimal_grid()+
-  #       theme_bw()
-  #     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 20), 
-  #           axis.text.y = element_text(size = 20), 
-  #           # legend.position = "none",
-  #           legend.title = element_blank(),
-  #           legend.text = element_text(size = 20),
-  #           legend.key.size = unit(3,"line"),
-  #           plot.margin=unit(c(1,1,0,1),"cm"), 
-  #           strip.text.x = element_text(size = 12), 
-  #           axis.title=element_text(size=22))+
-  #       # theme_bw()+
-  #     # colour = val_plt_grp$troph_sep,
-  #     labs(x = "", y = "RMSE/sd")+
-  #     ylim(0,4.5)+
-  #     scale_fill_manual(labels = c("elevation", "structure", "combination"), 
-  #                       values = c("lightblue2", "mediumseagreen", "orange2"))
-  # 
-  #     
-  #     
-  #     
-  #     
-  #   # # grid.locator(unit="npc")
-  #   # plt_crds <- par( "plt" )
-  #   # v <- ggplotGrob(plt)
-  #   # v <- gtable_add_rows(v, unit(1, 'cm'), 2)
-  #   # v <- gtable_add_grob(v,
-  #   #                      list(rectGrob(gp = gpar(col = NA, fill = gray(0.8))),
-  #   #                           textGrob("best model performance:", gp = gpar(col = "black", fontsize = 22), 
-  #   #                                    x = unit(plt_crds[3], "npc"), 
-  #   #                                    y = unit(plt_crds[4], "npc"), vjust = 1, hjust = 0.9
-  #   #                                    # hjust = c(2,0)
-  #   #                           )),
-  #   #                      3, 5, 3, 9, name = paste(runif(2)))
-  #   # 
-  #   # grid.newpage()
-  #   # grid.draw(v)
-  # pdf(file = paste0(outpath, set_dir, "mix/val_plot_srt_bestmodel_", comm, n, i, ".pdf"), height= 10, 
-  #     width = width)
-  # # par(mar=c(50, 50, 50, 50) + 1)#, paper = "a4r")
-  # print(plt)
-  # # print(grid.draw(v))
-  # dev.off()
-  
-  # }
-  
-  
 #####
 ###comparing RMSE of only residuals
 #####
@@ -496,23 +330,12 @@ n <- "RMSEsd_"
   levels(val_plt_res$resp) <- levels(val_type$resp)
   levels(val_plt_res$diet) <- levels(val_type$diet)
   
-  # val_plt_res$troph_sep <- droplevels(val_plt_res$troph_sep)
   val_plt_res$color <- droplevels(val_plt_res$color)
-  # val_plt_res$resp <- droplevels(val_plt_res$resp)
   val_plt_res$diet <- droplevels(val_plt_res$diet)
   
   table_res <- aggregate(val_plt_res$value, by = list(val_plt_res$Taxon, val_plt_res$type), FUN = median)
   colnames(table_res) <- c("Taxon", "type", "mdn_value")
   unq_sd <- data.frame(val_plt_res[,c("Taxon", "sd")])[!duplicated(data.frame(val_plt_res[,c("Taxon", "sd")])),]
-  
-  # table_res_sd <- merge(table_res, unq_sd, by = "Taxon", all = F)
-  # if (file.exists(paste0(inpath, set_dir, "mix/data/"))==F){
-  #   dir.create(file.path(paste0(inpath, set_dir, "mix/data/")), recursive = T)
-  # }
-  # write.csv(table_res_sd, file = paste0(inpath, set_dir, "mix/data/validation_table_res_", comm, n, ".csv"))
-  # 
-  
-  
   
   order_fun_res <- function(dat){
     unq_Taxon <- unique(data.frame(Tax_label = dat$Tax_label, RMSEsd_lidarRES_mdn = dat$RMSEsd_lidarRES_mdn))
@@ -520,7 +343,6 @@ n <- "RMSEsd_"
     dat$Tax_label <- factor(dat$Tax_label, levels = rev(srt_Taxon$Tax_label))
     return(dat)
   }
-  
   
   plt_fun_res <- function(dat){
     plt <- 
@@ -536,28 +358,12 @@ n <- "RMSEsd_"
             axis.ticks.y = element_blank(), 
             axis.ticks.x = element_blank(),
             axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)))+
-      # colour = val_plt_grp$troph_sep,
       labs(x = "", y = "RMSE/sd")+
       ylim(0,9)+
-      # scale_y_continuous(limits = c(0,4.5))+
       scale_fill_manual(labels = c("residuals"), values = "gray50")
     return(plt)
   }
-  
-    # for (i in grp){
-    #   #i <- "specs"
-    #   if(i == "specs"){
-    #     val_plt_grp <- val_plt_res[!val_plt_res$Taxon %in% trophs,]
-    #     width = 7
-    #   } else if(i == "trophs"){
-    #     width = 3
-    #     val_plt_grp <- val_plt_res[val_plt_res$Taxon %in% trophs,]
-    #   }
-    #   
-    #   unq_Taxon <- unique(data.frame(Tax_label = val_plt_grp$Tax_label, RMSEsd_lidarRES_mdn = val_plt_grp$RMSEsd_lidarRES_mdn))
-    #   srt_Taxon <- unq_Taxon[order(unq_Taxon$RMSEsd_lidarRES_mdn),]
-    #   val_plt_grp$Tax_label <- factor(val_plt_grp$Tax_label, levels = rev(srt_Taxon$Tax_label))
-    # 
+ 
       # specs
       specs_ord_res <- order_fun_res(dat = val_plt_res[!val_plt_res$Taxon %in% trophs,])
       specs_plt_res <- plt_fun_res(dat = specs_ord_res)
@@ -576,26 +382,3 @@ n <- "RMSEsd_"
       print(trophs_plt_res)
       dev.off()
     
-    # grid.locator(unit="npc")
-    # plt_crds <- par( "plt" )
-    # v <- ggplotGrob(plt)
-    # v <- gtable_add_rows(v, unit(0.75, 'cm'), 2)
-    # v <- gtable_add_grob(v,
-    #                      list(rectGrob(gp = gpar(col = NA, fill = gray(0.8))),
-    #                           textGrob("best model performance:", gp = gpar(col = "black"), x = unit(plt_crds[3], "npc"), 
-    #                                    y = unit(plt_crds[4], "npc"), vjust = 1.3, hjust = 1.55
-    #                                    # hjust = c(2,0)
-    #                           )),
-    #                      3, 5, 3, 9, name = paste(runif(2)))
-    # 
-    # grid.newpage()
-  #   # grid.draw(v)
-  #   pdf(file = paste0(outpath, set_dir, "mix/val_plot_residuals_", comm, n, i, ".pdf"), height= 10, 
-  #       width = width)
-  #   # par(mar=c(50, 50, 50, 50) + 1)#, paper = "a4r")
-  #   # print(plt)
-  #   print(plt)
-  #   dev.off()
-  #   
-  # }
-
